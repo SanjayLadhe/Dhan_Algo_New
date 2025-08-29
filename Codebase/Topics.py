daed742fbaf73a784ab94a191d14adec2d9ff25c
Sanjay Ladhe <sanjay.ladhe@ril.com>
2025-08-29 12:03:15 +0530

    New Addition

---
+++ Codebase/Topics.py
@@ -0,0 +1,25 @@
+Buy OTM
+take no of lots accoring to the VBPS ( keep 2 atr SL )
+Keep No targets at all...
+Trail SL, and Keep the TSL DYNAMIC at 2 ATR
+
+
+Order slicing above freeze quantity
+How to get margin required to place a order
+Add max no of orders the algo can take
+
+
+"Keep the points to seak, Hyperbolic discounting.."
+	smaller, immediate rewards over larger, delayed rewards
+
+	child : candy
+	we amazon : imdmeaite orders
+	trades  : now book targets, not later profit
+
+
+
+
+kurtosis fat tail returns
+"and how to be fearless in booking targets, sleep"
+
+
